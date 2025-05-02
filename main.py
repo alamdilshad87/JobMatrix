1e72e2cc990baec827a022f9a13fa97d3b272de6
saatvik-29 <121422100+saatvik-29@users.noreply.github.com>
2025-05-03 00:27:29 +0530

    fix7

--- main.py
+++ main.py
@@ -157,4 +157,5 @@ def results():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
